542112c9e42f4e7f2236d4c61d060e2442cbe3bc
mateusmacial <mateus_macial@hotmail.com>
2021-12-09 14:00:14 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/mateusmacial/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.mateusmacial.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mateusmacial.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}

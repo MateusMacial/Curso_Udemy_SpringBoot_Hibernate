93f76897c5d0ac08cd372531c0501447095032d4
Mateus Macial <mateus_macial@hotmail.com>
2021-12-12 11:55:44 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/mateusmacial/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.mateusmacial.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateusmacial.cursomc.domain.Cliente;
+import com.mateusmacial.cursomc.repositories.ClienteRepository;
+import com.mateusmacial.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}

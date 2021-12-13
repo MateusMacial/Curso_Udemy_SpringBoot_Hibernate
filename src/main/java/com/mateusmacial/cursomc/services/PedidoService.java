42aca7082d489b3196d9776f1b5c3e5fa6b4420d
mateusmacial <mateus_macial@hotmail.com>
2021-12-13 10:31:24 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/mateusmacial/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.mateusmacial.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateusmacial.cursomc.domain.Pedido;
+import com.mateusmacial.cursomc.repositories.PedidoRepository;
+import com.mateusmacial.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}

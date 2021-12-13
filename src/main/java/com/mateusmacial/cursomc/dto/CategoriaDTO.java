e10485df2943c1a72828e8eaa7ab46f52c0b3c04
mateusmacial <mateus_macial@hotmail.com>
2021-12-13 13:44:12 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/mateusmacial/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/mateusmacial/cursomc/dto/CategoriaDTO.java
@@ -2,17 +2,24 @@ package com.mateusmacial.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.mateusmacial.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
-	
+
 	public CategoriaDTO() {
 	}
-	
+
 	public CategoriaDTO(Categoria obj) {
 		this.id = obj.getId();
 		this.nome = obj.getNome();
@@ -21,6 +28,7 @@ public class CategoriaDTO implements Serializable {
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
@@ -28,9 +36,9 @@ public class CategoriaDTO implements Serializable {
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
 }
--- src/main/java/com/mateusmacial/cursomc/services/CategoriaService.java
+++ src/main/java/com/mateusmacial/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mateusmacial.cursomc.domain.Categoria;
+import com.mateusmacial.cursomc.dto.CategoriaDTO;
 import com.mateusmacial.cursomc.repositories.CategoriaRepository;
 import com.mateusmacial.cursomc.services.exceptions.DataIntegrityException;
 import com.mateusmacial.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }

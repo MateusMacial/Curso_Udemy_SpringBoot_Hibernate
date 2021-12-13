4dde9f1fae2004ec6ad526d86594ca65a34965fc
mateusmacial <mateus_macial@hotmail.com>
2021-12-13 09:20:23 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/mateusmacial/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.mateusmacial.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mateusmacial.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+	
+}

b1b515d303247c70423a7b3008fe9b01e59b7495
mateusmacial <mateus_macial@hotmail.com>
2021-12-13 14:05:50 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/mateusmacial/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,21 @@
+package com.mateusmacial.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long tmeStamp) {
+		super(status, msg, tmeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}

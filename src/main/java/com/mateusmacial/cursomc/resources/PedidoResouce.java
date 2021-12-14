2b3d18a330261a947f04446b558b414cddb82c07
mateusmacial <mateus_macial@hotmail.com>
2021-12-14 10:16:15 -0300

    Inserindo Pedido

--- src/main/java/com/mateusmacial/cursomc/resources/PedidoResouce.java
+++ src/main/java/com/mateusmacial/cursomc/resources/PedidoResouce.java
@@ -1,13 +1,22 @@
 package com.mateusmacial.cursomc.resources;
 
+import java.net.URI;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mateusmacial.cursomc.domain.Categoria;
 import com.mateusmacial.cursomc.domain.Pedido;
+import com.mateusmacial.cursomc.dto.CategoriaDTO;
 import com.mateusmacial.cursomc.services.PedidoService;
 
 @RestController
@@ -21,5 +30,17 @@ public class PedidoResouce {
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {		
 		Pedido obj = service.find(id);		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@Transactional
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){ 
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
